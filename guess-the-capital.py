b7e8caac00d9927ef9aa293d451c7c5c1dd39eaf
dsk0057 <dkafle6141@gmail.com>
2022-09-23 15:05:25 -0400

    Final version

--- guess-the-capital.py
+++ guess-the-capital.py
@@ -1,74 +1,80 @@
 import csv
 import random
-import crayons
+from colorama import Fore, Back, Style
 
-# file = open("countries.csv")
-#
-# csvreader = csv.reader(file)
-#
-# rows = []
-#
-# for row in csvreader:
-#     rows.append(row)
-#
-# country_capital_dict = {x[0]:x[1] for x in rows}
-# print(country_capital_dict)
-
-dict_country_capital = {}
-
-with open("countries.csv", mode="r") as data:
+# read the csv file and return a reader object
+with open("countries.csv", "r") as data:
     reader = csv.reader(data)
+
+    # iterate through reader and put the data in dictionary form
     dict_country_capital = {rows[0]: rows[1] for rows in reader}
+    print(dict_country_capital)
 
+# convert the dictionary into list for easier manipulation
 country_capital_list = list(dict_country_capital.items())
-print(country_capital_list)
 random_entry = random.choice(country_capital_list)
-print(random_entry)
-print(random_entry[0])
-print(random_entry[1])
 
-# (len(dict_country_capital)) -------> 247
+# Print welcome statement using coloroma's Fore, Back and Style properties
+print()
+print(f"{Fore.RED}{Back.CYAN}{Style.BRIGHT}-------------Welcome to the Capital Quiz Game--------------")
+print(Style.RESET_ALL)
 
-print("Would you like to play 'Capital-Quiz'?, Yes(y) or No(n)?: ")
+# declare variable for ease of use later
+total_countries = (len(dict_country_capital))
+# counter for number of attempts
 attempts = 0
 user_choice = ""
 
 
+# function for a game of quiz if user inputs "y"
 def playquiz():
     global attempts
+    # while loop to keep on prompting user until 3 attempts are reached
     while attempts < 3:
-        answer = input(f"You get 3 tries to get it right. What is the capital of {random_entry[0]}?: \n")
-        answer = answer.lower()
+        answer = input(f"{Fore.RED}What is the capital of {Fore.GREEN}{random_entry[0]}?: \n{Fore.RESET}")
+        answer = answer.lower().strip()
         attempts = attempts + 1
+        # check user answer and display if correct and terminate
         if answer == random_entry[1].lower():
-            print("Yay! Correct Answer!")
+            print(f"{Fore.GREEN}Yay! Correct Answer!")
             break
+        # output the correct answer once 3 incorrect attempts have been made
         if attempts == 3:
-            print(f"Good try, but you didn't get it! The correct answer was {random_entry[1]}")
+            print(f"{Fore.RED}\nGood try, but not quite! The correct answer was {Fore.GREEN}{random_entry[1]}.")
             break
         else:
-            print(f"{crayons.red('Incorrect answer')}, please try again.")
+            print(f"{Fore.RED}Incorrect answer, please try again.")
         # attempts = attempts + 1
 
 
+# main function that gets triggered initially during runtime
 def main():
     global attempts, user_choice
+    # while loop to keep on looping until input is n
     while user_choice != "n":
+        print(f"\n{Fore.YELLOW}Would you like to play?")
         print("\nEnter y for Yes.")
-        print("Enter n for No.")
-
-        user_choice = input("Yes or No?")
+        print(f"Enter n for No.{Fore.RESET}")
+        user_choice = input()
+        # input normalization
         user_choice = user_choice.lower().strip()
+        # test different conditions based on user input and display accordingly
         if user_choice == "y":
+            print(
+                f"{Fore.BLACK}{Back.LIGHTYELLOW_EX}You will get a random country from a list of {total_countries} countries.")
+            print("You get 3 attempts to guess the capital. \nAre you ready to test your geo-knowledge?")
+            print(Style.RESET_ALL)
             playquiz()
             break
         elif user_choice == "n":
-            # print("Thank you for visiting. Good bye!")
             break
         else:
-            print("Invalid input. Try again!")
+            print(f"{Fore.RED}{Style.BRIGHT}Invalid input. Try again!")
 
-    print("\nThank you for visiting! Bye for now.")
+    # end message for user leaving the game
+    print(f"\n{Fore.CYAN}Thank you for visiting! Bye for now.")
 
 
-main()
+# call the main function
+if __name__ == "__main__":
+    main()
